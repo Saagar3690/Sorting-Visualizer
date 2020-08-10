84b14e50ccfda3c06ddf35d379d1f866eb4b3ee1
Saagar <saagar.mahadev123@gmail.com>
2020-08-10 16:44:11 -0700

    First Commit

--- algorithms.py
+++ algorithms.py
@@ -1,61 +1,106 @@
 import random
 
+# Bubble Sort
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
 def bubblesort(window, tk, data):
+  # iteration counter
   iterations = 0
+  # boolean for if swaps occurred
   swapped = True
+  # end of traversal counter
   end = len(data)-1
+  # while there has been a swap
   while swapped:
     swapped = False
+    # traverse the list from 0 to where you ended last time (end)
     for i in range(end):
+      # increment iterations
       iterations += 1
+      # check if elements are out of order (current element is bigger than the next element)
       if data[i] > data[i+1]:
+        # swap
         data[i], data[i+1] = data[i+1], data[i]
         swapped = True
+        # redraw graph
         window.draw_graph(data, [i ,i+1])
         tk.update()
+    # decrement the end as everything after it is already in order
     end -= 1
 
   return iterations
 
+# Selection Sort
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
 def selectionsort(window, tk, data):
+  # iteration counter
   iterations = 0
+  # traverse all elements
   for i in range(len(data)-1):
+    # set location of minimum to ith element
     min = i
+    # traverse from i+1 to end of list as everything before is already in order
     for j in range(i+1, len(data)):
+      # increment iterations
       iterations += 1
+      # if current element is smaller than the minimum element, set location of min to current element
       if data[j] < data[min]:
         min = j
+    # swap min element with first found element
     data[i], data[min] = data[min], data[i]
+    # redraw graph
     window.draw_graph(data, [i, min])
     tk.update()
 
   return iterations
 
+# Insertion Sort
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
 def insertionsort(window, tk, data):
+  # iteration counter
   iterations = 0
+  # traverse 1 through end of list
   for i in range(1, len(data)):
+    # current element
     cur = data[i]
     j = i-1
+    # move elements of list that are greater than the current element up one position
     while j >= 0 and cur < data[j]:
+      # increment iterations
       iterations += 1
       data[j+1] = data[j]
+      # redraw graph
       window.draw_graph(data, [j+1, j])
       tk.update()
       j -= 1
+    # put current element in proper position
     data[j+1] = cur
+    # redraw graph
     window.draw_graph(data, [j+1, i])
     tk.update()
 
   return iterations
 
-# iterative
+# Iterative Merge Sort
+# Time Complexity: O(nlogn)
+# Space Complexity: O(1)
 def mergesort(window, tk, data, low=0, high=34):
+  # temp list with same values as data
   temp = [data[i] for i in range(35)]
+  # iteration counter
   iterations = 0
   m = 1
+  # divide array into blocks of size m
   while m <= high - low:
+    # for m = 1, i = 0, 2, 4, 6, 8
+    # for m = 2, i = 0, 4, 8
+    # for m = 3, i = 0, 8
     for i in range(low, high, 2*m):
+      # increment iterations
       iterations += 1
+      # left, mid, right
       left = i
       mid = i + m - 1
       right = min(i + 2*m - 1, high)
@@ -66,13 +111,18 @@ def mergesort(window, tk, data, low=0, high=34):
 
   return iterations
 
+# Helper method for Iterative Merge Sort
 def merge(window, tk, data, temp, left, mid, right):
+  # iteration counter
   iterations = 0
   k, i, j = left, left, mid + 1
 
+  # traverse while there are elements in left and right side
   while i <= mid and j <= right:
+    # redraw graph
     window.draw_graph(data, [i, j])
     tk.update()
+    # compare left and right side and store smaller value into temp list
     if data[i] < data[j]:
       temp[k] = data[i]
       i += 1
@@ -80,35 +130,49 @@ def merge(window, tk, data, temp, left, mid, right):
       temp[k] = data[j]
       j += 1
     k += 1
+    # increment iterations
     iterations += 1
 
+  # add remaining elements to list
   while i < 34 and i <= mid:
+    # redraw graph
     window.draw_graph(data, [i])
     tk.update()
+    # increment iterations
     iterations += 1
     temp[k] = data[i]
     i += 1
     k += 1
 
+  # copy from temp list to original data array
   for i in range(left, right+1):
     data[i] = temp[i]
 
+  # redraw graph
   window.draw_graph(data)
   tk.update()
 
   return iterations
 
-'''# recursive
+'''
+# Recursive Merge Sort
+# Time Complexity: O(nlogn)
+# Space Complexity: O(n)
 def mergesort(window, tk, data, start=0, end=35, level=1, iterations=0):
+  # base case: only one element in the list
   if len(data) == 1:
     return iterations, data
 
+  # calculate middle
   middle = len(data)//2
+  # recursively call left side
   its, left = mergesort(window, tk, data[:middle], start, middle, level+1)
   iterations += its
+  # recursively call right side
   its, right = mergesort(window, tk, data[middle:], middle, end, level+1)
   iterations += its
 
+  # merge left and right
   its, merged_data = merge(window, tk, left, right)
   iterations += its
   if level != 1:
@@ -119,13 +183,20 @@ def mergesort(window, tk, data, start=0, end=35, level=1, iterations=0):
     tk.update()
     return iterations
 
+# Helper method for Recursive Merge Sort
 def merge(window, tk, left, right):
+  # iteration counter
   iterations = 0
   data = []
+
+  # traverse while there are elements in left and right side
   while len(left) > 0 and len(right) > 0:
+    # increment iterations
     iterations += 1
+    # redraw graph
     window.draw_graph(window.data, [left[0], right[0]])
     tk.update()
+    # compare left and right side and store smaller value into temp list
     if left[0] < right[0]:
       data.append(left[0])
       left.pop(0)
@@ -133,245 +204,270 @@ def merge(window, tk, left, right):
       data.append(right[0])
       right.pop(0)
 
+  # add any remaining elements
   if len(left) == 0:
     data.extend(right)
   else:
     data.extend(left)
 
-  return iterations, data'''
+  return iterations, data
+'''
 
+# Shell Sort
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
 def shellsort(window, tk, data):
+  # iteration counter
   iterations = 0
+  # big gap
   gap = len(data)//2
 
+  # gapped insertion sort
+  # The first gap elements a[0..gap-1] are already in gapped order keep adding one more element until the entire array is gap sorted
   while gap > 0:
     for i in range(gap, len(data)):
+      # increment iterations
       iterations += 1
+      # current element
       cur = data[i]
 
+      # shift earlier gap-sorted elements up until the correct
+      # location for a[i] is found
       j = i
       while j >= gap and data[j-gap] > cur:
         data[j] = data[j-gap]
+        # redraw graph
         window.draw_graph(data, [j, j-gap])
         tk.update()
         j -= gap
 
+      # put current element in its correct location
       data[j] = cur
+      # redraw graph
       window.draw_graph(data, [j, i])
       tk.update()
+
+    # make gap smaller
     gap //= 2
 
   return iterations
 
+# Quick Sort
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
 def quicksort(window, tk, data, low, high, iterations=0):
   if low < high:
+    # partitioning index, so that data[pi] is in the correct place
     pi, its = partition(window, tk, data, low, high)
     iterations += its
+    # sort elements before and after partition
     iterations += quicksort(window, tk, data, low, pi-1)
     iterations += quicksort(window, tk, data, pi+1, high)
 
   return iterations
 
+# Helper method for Quick Sort
 def partition(window, tk, data, low, high):
+  # iteration counter
   iterations = 0
+  # pivot
   pivot = data[high]
+  # index of smaller element
   i = low-1
 
   for j in range(low, high):
+    # increment iterations
     iterations += 1
+    # if current element is smaller than the pivot
     if data[j] < pivot:
+      # increment index of smaller element
       i += 1
+      # swap
       data[i], data[j] = data[j], data[i]
+      # redraw graph
       window.draw_graph(data, [i, j])
       tk.update()
   data[i+1], data[high] = data[high], data[i+1]
+  # redraw graph
   window.draw_graph(data, [i+1, high])
   tk.update()
 
   return (i+1), iterations
 
+# Heap Sort
+# Time Complexity: O(nlogn)
+# Space Complexity: O(1)
 def heapsort(window, tk, data):
+  # iteration counter
   iterations = 0
+
+  # build a maxheap
   for i in range(len(data)//2-1, -1, -1):
+    # increment iterations
     iterations += 1
     heapify(window, tk, data, len(data), i)
 
+  # extract elements one by one
   for i in range(len(data)-1, 0, -1):
+    # increment iterations
     iterations += 1
+    # swap
     data[i], data[0] = data[0], data[i]
+    # redraw graph
     window.draw_graph(data, [0, i])
     tk.update()
     heapify(window, tk, data, i, 0)
 
   return iterations
 
+# Helper method for Heap Sort
 def heapify(window, tk, data, n, i):
+  # largest is the root
   largest = i
+  # left
   l = 2 * i + 1
+  # right
   r = 2 * i + 2
 
+  # if left child exists and is greater than root
   if l < n and data[i] < data[l]:
     largest = l
+  # if right child exists and is greater than root
   if r < n and data[largest] < data[r]:
     largest = r
+  # change root if necessary
   if largest != i:
     data[i], data[largest] = data[largest], data[i]
+    # redraw graph
     window.draw_graph(data, [i, largest])
     tk.update()
 
+    # heapify root
     heapify(window, tk, data, n, largest)
 
+# Comb Sort
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
 def combsort(window, tk, data):
+  # iteration counter
   iterations = 0
+  # initialize gap
   gap = len(data)
+  # boolean for if swaps have been made
   swapped = True
 
+  # keeps running while gap is more than 1 or last iteration had a swap
   while gap != 1 or swapped:
     swapped = False
+    # get next gap
     gap = getNextGap(gap)
 
+  # compare all elements with current c\gap
     for i in range(len(data)-gap):
+      # increment iterations
       iterations += 1
+      # compare ith and ith+gap elements
       if data[i] > data[i+gap]:
+        # swap
         data[i], data[i+gap] = data[i+gap], data[i]
+        # redraw graph
         window.draw_graph(data, [i, i+gap])
         tk.update()
         swapped = True
 
   return iterations
 
+# Helper method for Comb Sort
 def getNextGap(gap):
+  # shrink gap factor by 1.3
   gap = int(gap/1.3)
   if gap < 1:
     return 1
   return gap
 
+# Cocktail Sort
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
 def cocktailsort(window, tk, data):
+  # iteration counter
   iterations = 0
+  # boolean for if swaps have been made
   swapped = True
+  # start of traversal
   start = 0
+  # end of traversal
   end = len(data) - 1
 
+  # while swaps have occurred
   while swapped:
+    # reset swapped to False
     swapped = False
 
+    # traverse left to right like bubble sort
     for i in range(start, end):
+      # increment iterations
       iterations += 1
+      # compare elements - ith and ith+1
       if data[i] > data[i+1]:
+        # swap
         data[i], data[i+1] = data[i+1], data[i]
+        # redraw graph
         window.draw_graph(data, [i, i+1])
         tk.update()
         swapped = True
 
+    # if no swaps occurred, list is already sorted, so break out of loop
     if not swapped:
       break
 
+    # reset swapped to False
     swapped = False
+    # decrement end as everything after it is in the right place
     end -= 1
 
+    # traverse right to left
     for i in range(end, start, -1):
+      # increment iterations
       iterations += 1
+      # compare elements - ith and ith-1
       if data[i] < data[i-1]:
+        # swap
         data[i], data[i-1] = data[i-1], data[i]
+        # redraw graph
         window.draw_graph(data, [i, i-1])
         tk.update()
         swapped = True
 
+    # increment start as everything before it is in the right place
     start += 1
 
   return iterations
 
+# Monkey Sort
+# Time Complexity: O(infinity)
+# Space Complexity: O(n)
 def monkeysort(data):
+  # if data is not sorted, generate a permutation of the data
   while not is_sorted(data):
     random.shuffle(data)
 
   return data
 
+# Helper method for Monkey Sort
 def is_sorted(data):
+  # traverse through all elements
   for i in range(len(data)-1):
+    # if ith and ith+1 elements are out place, list is not sorted, so return False
     if data[i] > data[i+1]:
       return False
 
   return True
 
+# Generate a list of numbers 1-35 in random order
 def generate_data():
-  data = []
-  for i in range(1, 36):
-    data.append(i)
+  # initialize data to 1-35
+  data = [i for i in range(1, 36)]
+  # shuffle the order of elements
   random.shuffle(data)
 
   return data
-
-
-
-
-
-'''data_to_be_sorted = []
-for i in range(1, 51):
-  data_to_be_sorted.append(i)
-random.shuffle(data_to_be_sorted)
-data_sorted = bubblesort(data_to_be_sorted)
-print('Bubble Sort: ' + str(data_sorted))
-
-data_to_be_sorted = []
-for i in range(1, 51):
-  data_to_be_sorted.append(i)
-random.shuffle(data_to_be_sorted)
-data_sorted = selectionsort(data_to_be_sorted)
-print('Selection Sort: ' + str(data_sorted))
-
-data_to_be_sorted = []
-for i in range(1, 51):
-  data_to_be_sorted.append(i)
-random.shuffle(data_to_be_sorted)
-data_sorted = insertionsort(data_to_be_sorted)
-print('Insertion Sort: ' + str(data_sorted))
-
-data_to_be_sorted = []
-for i in range(1, 51):
-  data_to_be_sorted.append(i)
-random.shuffle(data_to_be_sorted)
-data_sorted = mergesort(data_to_be_sorted)
-print('Merge Sort: ' + str(data_sorted))
-
-data_to_be_sorted = []
-for i in range(1, 51):
-  data_to_be_sorted.append(i)
-random.shuffle(data_to_be_sorted)
-data_sorted = shellsort(data_to_be_sorted)
-print('Shell Sort: ' + str(data_sorted))
-
-data_to_be_sorted = []
-for i in range(1, 51):
-  data_to_be_sorted.append(i)
-random.shuffle(data_to_be_sorted)
-data_sorted = quicksort(data_to_be_sorted, 0, len(data_to_be_sorted)-1)
-print('Quick Sort: ' + str(data_sorted))
-
-data_to_be_sorted = []
-for i in range(1, 51):
-  data_to_be_sorted.append(i)
-random.shuffle(data_to_be_sorted)
-data_sorted = heapsort(data_to_be_sorted)
-print('Heap Sort: ' + str(data_sorted))
-
-data_to_be_sorted = []
-for i in range(1, 51):
-  data_to_be_sorted.append(i)
-random.shuffle(data_to_be_sorted)
-data_sorted = combsort(data_to_be_sorted)
-print('Comb Sort: ' + str(data_sorted))
-
-data_to_be_sorted = []
-for i in range(1, 51):
-  data_to_be_sorted.append(i)
-random.shuffle(data_to_be_sorted)
-data_sorted = cocktailsort(data_to_be_sorted)
-print('Cocktail Sort: ' + str(data_sorted))
-
-data_to_be_sorted = []
-for i in range(1, 51):
-  data_to_be_sorted.append(i)
-random.shuffle(data_to_be_sorted)
-data_sorted = monkeysort(data_to_be_sorted)
-print('Monkey Sort: ' + str(data_sorted))'''
--- ui.py
+++ ui.py
@@ -17,115 +17,60 @@ class UI(Frame):
     self.data = data
     self.__initUI()
 
-  # initialize UI and draw the board
+  # initialize UI and draw the graphs
   def __initUI(self):
     self.parent.title('Sorting Visualizer')
     self.grid(row=self.row, column=self.col)
     # canvas for the board to be drawn
     self.canvas = Canvas(self, bg=self.color, width=WIDTH, height=HEIGHT)
     self.canvas.pack(fill=BOTH, side=TOP)
+    # generate data
     self.data = algorithms.generate_data()
+    # draw the graph
     self.draw_graph(self.data)
+    # algorithm label
     label = Label(self, text=self.algorithm)
     label.pack()
+    # start button
     start = Button(self, text='Start', command=self.__run_algorithm, bd=30)
     start.pack()
 
   def __run_algorithm(self):
+    # time start
     start = time.time()
+    # call algorithm function
     if self.algorithm != 'Quick Sort':
       iterations = self.algorithmCall(self, self.parent, self.data)
     else:
       iterations = self.algorithmCall(self, self.parent, self.data, 0, len(self.data)-1)
+    # total time for algorithm to run
     totalTime = time.time() - start
+    # update title to total time and number of iterations
     self.parent.title('Algorithm: %s\t\tTime: %fs\t\tIterations: %d' % (self.algorithm, totalTime, iterations))
+    # after 4 seconds...
     self.after(4000)
+    # clear the graph
     self.draw_graph(self.data, done=True)
+    # generate new data
     self.data = algorithms.generate_data()
+    # draw new graph
     self.draw_graph(self.data)
+    # reset title
     self.parent.title('Sorting Visualizer')
 
   def draw_graph(self, data, cur=[], done=False):
+    # clear previous version of graph
     self.canvas.delete('all')
+    # for all elements in graph
     for i in range(35):
+      # if the algorithm has already run, show the graph being cleared and a new graph being generated
       if done:
         self.canvas.create_rectangle(8*i + MARGIN * 3, HEIGHT - 8 * data[i], 8*(i+1) + MARGIN * 3, HEIGHT, fill='black', outline='white')
         self.parent.update()
         self.after(30)
       else:
+        # make current bar in graph white if it is one of the bars whose position was just swapped, else add a normal bar
         if i in cur:
           self.canvas.create_rectangle(8*i + MARGIN * 3, HEIGHT - 8 * data[i], 8*(i+1) + MARGIN * 3, HEIGHT, fill='white', outline='black')
         else:
           self.canvas.create_rectangle(8*i + MARGIN * 3, HEIGHT - 8 * data[i], 8*(i+1) + MARGIN * 3, HEIGHT, outline='black')
-
-  '''# redraw the board after the value of a cell changes
-  def redraw(self, board, row, col, iterations=0, time=0):
-    # time and iterations
-    self.parent.title('Time: %fs\tIterations: %d' % (time, iterations))
-    # reset canvas
-    self.canvas.delete('all')
-    # redraw the board
-    self.__draw_board(board, row, col)
-
-  # draw the board
-  def __draw_board(self, board, row=-1, col=-1):
-    self.__draw_cells(row, col)
-    self.__draw_grid()
-    self.__draw_values(board)
-
-  # draw the grid
-  def __draw_grid(self):
-    # 10 lines
-    for i in range(10):
-      # black for main lines that separate board into 3x3, gray for other sublines
-      color = "black" if i % 3 == 0 else "gray"
-
-      # Horizontal Lines
-      x0 = MARGIN
-      y0 = MARGIN + i * SIDE
-      x1 = WIDTH - MARGIN
-      self.canvas.create_line(x0, y0, x1, y0, fill=color)
-
-      # Vertical Lines
-      x0 = MARGIN + i * SIDE
-      y0 = MARGIN
-      y1 = HEIGHT - MARGIN
-      self.canvas.create_line(x0, y0, x0, y1, fill=color)
-
-  # draw the cells
-  def __draw_cells(self, row=-1, col=-1):
-    # column
-    for i in range(9):
-      # row
-      for j in range(9):
-        x0 = MARGIN + i * SIDE
-        y0 = MARGIN + j * SIDE
-        x1 = MARGIN + (i+1) * SIDE
-        y1 = MARGIN + (j+1) * SIDE
-        if col != i or row != j:
-          # all other cells
-          self.canvas.create_rectangle(x0, y0, x1, y1, outline='gray')
-        else:
-          # current cell
-          self.canvas.create_rectangle(x0, y0, x1, y1, outline='red', width=3)
-
-  # draw the values in the cells
-  def __draw_values(self, board):
-    # delete the values the solver generated
-    self.canvas.delete('numbers')
-    # row
-    for i in range(9):
-      # column
-      for j in range(9):
-        # center of cell
-        x = MARGIN + j * SIDE + SIDE / 2
-        y = MARGIN + i * SIDE + SIDE / 2
-        # temporary variable for current coordinates
-        tmp = [i, j]
-        # if current coordinates is  part of the clues (the given values at the beginning), color of values of clues is black
-        if not tmp in self.clues:
-          # if current cell's value is not empty (0), color of the values are gray
-          if board[i][j] != 0:
-            self.canvas.create_text(x, y, text=board[i][j], tags='numbers', fill='gray')
-        else:
-          self.canvas.create_text(x, y, text=board[i][j], tags='clues', fill='black')'''
